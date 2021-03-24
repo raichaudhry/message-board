5c467e5efb4ca0841a627d2e70e653af5e3ff65f
Rai Chaudhry <72818839+raichaudhry@users.noreply.github.com>
2021-03-24 14:52:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,17 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Message Board!</title>
+    <link rel="stylesheet" href="index.css">
+    <title>Message Board</title>
 </head>
 <body>
-    <input id = "message"></input>
-    <input id = "name"></input>
-    <button onclick = "submit();">Submit</button>
+<div id = "topbar">
+    <h1>RAICHAUDHRY.COM BRINGS YOU...</h1>
+    <h2>Message Board Beta!</h2>
+</div>
+<div id = "userInput">
+<input type = "text" placeholder = "Message"id = "message"></input>
+<input type = "name" placeholder = "Name"id = "name"></input>
+<button onclick = "submit();">Submit</button>
+</div>
 <?php
 $file = fopen("txt.txt", "a") or die("Unable to open file!");
 $txt = "TEST THINGY\n";
 fwrite($myfile, $txt);
 fclose($myfile);
 ?>
+<script src = "index.js"></script>
 </body>
 </html>
